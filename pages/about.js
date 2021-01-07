34babd8a1c0d683a9451a03d8962eec3b3a25ae8
paul-leonard <paul.ryan.leonard@gmail.com>
2021-01-06 23:27:00 -0800

    basic components created, about js file created, eslint added to packagejson

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <>
+      {/* TODO: Add NavBar here */}
+      <h1>About</h1>
+      <p>This is a feed of wonderful webcomics.</p>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,53 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Clever Entertainment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        
+        <NavBar />
+        
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          XKCD Webcomic
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <TodayTitle />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <TodayComic />
+        
+        <h2>
+          Previous 10
+        </h2>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <PreviousList />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        {/* <p className={styles.description}>
+          Get started by editing
+        </p> */}
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+
       </footer>
     </div>
   )
+}
+
+function NavBar() {
+  return <nav>Home?</nav>
+}
+
+function TodayTitle() {
+  return <h2>Today Title</h2>
+}
+
+function TodayComic() {
+  return <p>pic of comic here</p>
+}
+
+function PreviousList() {
+  return <footer>List</footer>
 }
